683cc965b4fc355aa5e258a44325efffc82e2d80
alexxck <boguslavets@gmail.com>
2019-11-09 16:53:58 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'posts#index' #set start page localhost:3000
+  resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
