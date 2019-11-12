8bb34cd3eb793c1522731043a9289bb0afdda594
alexxck <boguslavets@gmail.com>
2019-11-12 16:41:12 +0200

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'posts#index' #set start page localhost:3000
+  root 'posts#index' # set start page localhost:3000
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
