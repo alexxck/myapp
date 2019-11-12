be26f7e8d3dbbf4ebbbff8781efef483112ecca9
alexxck <boguslavets@gmail.com>
2019-11-12 10:24:27 +0200

    + bootsrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :name, :title, presence: true,
-            length: { minimum: 5 }
+  validates :name, :title, presence: true, length: { minimum: 5 }
+
 end
