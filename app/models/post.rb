562bc786944619a8aff51f61d069e274999a426c
alexxck <boguslavets@gmail.com>
2019-11-18 20:40:06 +0200

    -bug in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :name, :title, presence: true, length: { minimum: 3 }
-  belongs_to :author, counter_cache: true
+  belongs_to :author
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
   is_impressionable
