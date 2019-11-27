5f7d024994f207b7e203d1085119aa1df2162423
alexxck <boguslavets@gmail.com>
2019-11-27 19:21:07 +0200

    fixed edited! for comment

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,7 +6,7 @@ class Author < ApplicationRecord
   has_many :comments, dependent: :destroy
   before_save { self.email = email.downcase }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   PASSWORD_FORMAT = /\A
   (?=.{8,})          # Must contain 8 or more characters
@@ -14,7 +14,7 @@ class Author < ApplicationRecord
   (?=.*[a-z])        # Must contain a lower case character
   (?=.*[A-Z])        # Must contain an upper case character
   (?=.*[[:^alnum:]]) # Must contain a symbol
-  /x
+  /x.freeze
   validates :password, presence: true, length: { minimum: 8 }, format: { with: PASSWORD_FORMAT }
 
   def full_name
