16832e06f14f3d2f38e4f642f0f8cbcca97004d7
alexxck <boguslavets@gmail.com>
2019-11-27 11:21:23 +0200

    regex for email&pass

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,8 +4,18 @@ class Author < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  before_save { self.email = email.downcase }
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  PASSWORD_FORMAT = /\A
+  (?=.{8,})          # Must contain 8 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+  (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: PASSWORD_FORMAT }
 
   def full_name
     "#{first_name} #{last_name}"
