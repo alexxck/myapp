434be8436bf51c588830502e1bb90f00d5d13e31
alexxck <boguslavets@gmail.com>
2019-12-10 10:42:35 +0200

    mailer fixed

--- app/mailers/author_mailer.rb
+++ app/mailers/author_mailer.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class AuthorMailer < ApplicationMailer
-  default :from => "application_name@domain.com"
+  default :from => 'myappblog1@gmail.com'
 
   def registration_confirmation(author)
     @author = author
-    mail(:to => "#{author.full_name} <#{author.email}", :subject => "confirm your registration on myapp blog")
+    mail to: author.email, subject: "Registration Confirmation on myapp blog"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,18 +50,6 @@ ActiveRecord::Schema.define(version: 2019_12_09_172507) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
